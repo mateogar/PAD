5ffe6a28a9a46bda87827a4a212495804b12324d
Aarón Durán <aarondur@ucm.es>
2017-04-05 19:53:41 +0200

    Primer acercamiento

---
+++ game.js
@@ -0,0 +1,13 @@
+var pulsar = function(id) {
+    console.log(id);
+    document.getElementById(id).style.background = '#000000';
+
+}
+
+/*
+Document.getbutton.onClick = function() {
+
+    console.log(button.id);
+}
+
+*/
